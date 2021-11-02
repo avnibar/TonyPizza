29e406168e1276b1ae3e3313c913c90a1e013e10
avnibar <82066521+avnibar@users.noreply.github.com>
2021-11-02 15:20:10 +1300

    Add files via upload

--- TonysPizza.py
+++ TonysPizza.py
@@ -3,6 +3,17 @@ from tkinter.font import Font
 from datetime import datetime, timedelta
 
 root = Tk()
+root.title("Tony's Pizza Order Form")
+root.config(bg="#ffe599")
+
+
+global regularCost
+regularCost = 8.5
+global gourmetCost
+gourmetCost = 13.5
+global deliveryCost
+deliveryCost = 3
+menuList = ["Cheese", "Pepperoni", "Meat Lovers", "Hawaiian", "BBQ Chicken", "Vegetable", "Mushroom", "Tuna (G)", "Triple Cheese (G)", "Margherita (G)", "Buffalo (G)", "Sausage (G)"] 
 
 
 
@@ -10,5 +21,17 @@ root = Tk()
 
 
 
+
+
+
+
+
+
+
+
+
+findInvalids()
+calcCost()
+createInvoice()
 
 root.mainloop()
